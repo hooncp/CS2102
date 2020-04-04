50b70f59559757dfe97625ad2e8f94e6c8949361
weidonglim <e0310944@comp.nus.edu.sg>
2020-04-04 15:29:27 +0800

    insert restaurant

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 const pool = require("../database/db");
 
+// Create a Customer
 router.post("/insertCustomers", async (req, res) => {
-  // console.log("succeed");
   const client = await pool.connect();
   try {
     let currId = 0;
@@ -28,6 +28,83 @@ router.post("/insertCustomers", async (req, res) => {
         });
     });
     return res.json(`${name} added as a Customer`);
+  } catch (err) {
+    client.query(`ROLLBACK`);
+    client.release();
+    console.error("error triggered: ", err.message);
+  }
+});
+
+// Create a Restaurant
+router.post("/insertRestaurant", async (req, res) => {
+  const client = await pool.connect();
+  try {
+    const rname = req.body.rname;
+    const minorderamt = req.body.minorderamt;
+    const area = req.body.area;
+    client.query("BEGIN").then((res) => {
+      client
+        .query(
+          `INSERT INTO restaurants (rname, minorderamt, area) VALUES ($1, $2, $3)`,
+          [rname, minorderamt, area]
+        )
+        .then((result) => {
+          client.query("COMMIT");
+          client.release();
+        });
+    });
+    return res.json(`${rname} added as a Restaurant`);
+  } catch (err) {
+    client.query(`ROLLBACK`);
+    client.release();
+    console.error("error triggered: ", err.message);
+  }
+});
+
+// Create a Food
+router.post("/insertFood", async (req, res) => {
+  const client = await pool.connect();
+  try {
+    const fname = req.body.fname;
+    const category = req.body.category;
+    client.query("BEGIN").then((res) => {
+      client
+        .query(`INSERT INTO restaurants (fname, category) VALUES ($1, $2)`, [
+          fname,
+          category,
+        ])
+        .then((result) => {
+          client.query("COMMIT");
+          client.release();
+        });
+    });
+    return res.json(`${fname} added as a Food`);
+  } catch (err) {
+    client.query(`ROLLBACK`);
+    client.release();
+    console.error("error triggered: ", err.message);
+  }
+});
+
+// Create a Customer
+router.post("/insertFoods", async (req, res) => {
+  const client = await pool.connect();
+  try {
+    const fname = req.body.fname;
+    const category = req.body.category;
+    client.query("BEGIN").then((res) => {
+      client
+        .query(`INSERT INTO food(fname, category) VALUES ($1, $2)`, [
+          fname,
+          category,
+        ])
+        .then((result) => {
+          client.query("COMMIT");
+          client.release();
+        });
+    });
+
+    return res.json(`${fname} added as a Food`);
   } catch (err) {
     client.query(`ROLLBACK`);
     client.release();
