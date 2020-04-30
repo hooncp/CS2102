3177e9a35c803abf2c536ee9724efc6d78b5e6d3
Leow jit yong <jityong@jityong.lan>
2020-04-30 13:58:40 +0800

    inital merge fds-2 with master

--- backend/routes/fds.js
+++ backend/routes/fds.js
@@ -267,6 +267,54 @@ router.get('/viewMonthRidersSummary', (req, res) => {
         .catch(e => console.error(e.stack))
 })
 
+//7. number of ratings received by rider for all orders delivered for that month
+router.get('/getMonthlyNumRating', async (req, res) => {
+    const month = req.body.month;
+    const query = `SELECT count(rating) as numRating, userId
+	FROM Delivers D
+	WHERE DATE_PART('MONTHS',D.deliveryTimetoCustomer) = ${month}
+	GROUP BY userId
+	;`
+    pool.query(query).then(result => {
+        let numRating = result.rows
+        console.log('num of ratings:', numRating);
+        res.json(numRating);
+    }).catch(err => {
+        if (err.constraint) {
+            console.error(err.constraint);
+        } else {
+            console.log(err);
+            res.json(err);
+        }
+    });
+
+})
+
+// 6. average delivery time by the rider for that month
+//departTimeForRestaurant - deliveryTimetoCustomer = delivery time
+// average delivery time = total delivery time / total num of orders [monthly]
+router.get('/getMonthlyAverageDeliveryTime', async (req, res) => {
+    const month = req.body.month;
+    const query = `select sum((extract(epoch from (deliveryTimetoCustomer - departTimeForRestaurant)))/60)
+		/count(userId) as avgTime, userId
+		FROM Delivers D
+		WHERE DATE_PART('months',D.deliveryTimetoCustomer) = ${month}
+		GROUP BY userId`
+    pool.query(query).then(result => {
+        let avgTime = result.rows
+        console.log('result:', avgTime);
+        res.json(avgTime);
+    }).catch(err => {
+        if (err.constraint) {
+            console.error(err.constraint);
+        } else {
+            console.log(err);
+            res.json(err);
+        }
+    });
+
+})
+
 //For each for each month and for each customer who has placed some order for that month,
 // the total number of orders placed by the customer for that month and the total cost of all these orders.
 router.get('/viewMonthlyCustomerSummary', (req, res) => {
@@ -289,7 +337,6 @@ router.get('/viewMonthlyCustomerSummary', (req, res) => {
 })
 
 
-module.exports = router;
 /*
 SELECT count(R.userId)
 FROM Riders R
@@ -299,8 +346,8 @@ WHERE checkWorkingStatusHelperOfRider(R.userId, '2020-11-12 11:10:00') = 1;
 // not tested yet
 router.get('/checkStatus', async (req, res) => {
     const timeArr = ["10:00:00", '11:10:00', "12:00:00", "13:00:00", "14:00:00"
-    , "15:00:00", "16:00:00", "17:00:00", "18:00:00", "19:00:00", "20:00:00",
-    "21:00:00", "22:00:00"]
+        , "15:00:00", "16:00:00", "17:00:00", "18:00:00", "19:00:00", "20:00:00",
+        "21:00:00", "22:00:00"]
     const day = req.body.day; //example 2020-11-12
     let querytime = "";
 
@@ -308,21 +355,21 @@ router.get('/checkStatus', async (req, res) => {
     //console.log('2020-11-12 11:10:00');
 
     let resArr = [];
-/*
-    pool.query(query).then(result => {
-        console.log('result:', result.rows[0].count);
-        res.json(result.rows[0].count);
-    }).catch(err => {
-        if (err.constraint) {
-            console.error(err.constraint);
-        } else {
-            console.log(err);
-            res.json(err);
-        }
-    });
-    */
+    /*
+        pool.query(query).then(result => {
+            console.log('result:', result.rows[0].count);
+            res.json(result.rows[0].count);
+        }).catch(err => {
+            if (err.constraint) {
+                console.error(err.constraint);
+            } else {
+                console.log(err);
+                res.json(err);
+            }
+        });
+        */
 
-    for(let i = 0; i < timeArr.length; i++) {
+    for (let i = 0; i < timeArr.length; i++) {
 
 
         querytime = day + " " + timeArr[i];
@@ -337,18 +384,19 @@ router.get('/checkStatus', async (req, res) => {
 
             console.log(resArr);
             await resArr.push(result.rows[0].count);
-            }).catch(err => {
-                if (err.constraint) {
-                    console.error(err.constraint);
-                } else {
-                    console.log(err);
-                    res.json(err);
-                }
+        }).catch(err => {
+            if (err.constraint) {
+                console.error(err.constraint);
+            } else {
+                console.log(err);
+                res.json(err);
+            }
         });
 
     }
     console.log("test" + resArr);
-    res.json({"data": resArr});
+    res.json({ "data": resArr });
 })
+module.exports = router;
 
 
